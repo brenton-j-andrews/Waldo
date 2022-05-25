6d509a8dba3e421203f797a7d097e719ef17ac66
brent <brenton.j.andrews@gmail.com>
2022-05-25 12:53:39 -0600

    Front end file structure established

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './Styles/App.css';
 
 function App() {
   return (
---
+++ src/Components/General/Header.js
@@ -0,0 +1,13 @@
+// Page Header Component
+
+import react from "react";
+
+const Header = (props) => {
+    return (
+        <div className="page-header">
+            This is the header.
+        </div>
+    )
+}
+
+export default Header;
