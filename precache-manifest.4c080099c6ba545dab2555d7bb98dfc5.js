df3c75f088f54d0c5ffc3de994b8cb99a568e95b
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-08-13 15:16:03 -0600

    Updates

---
+++ precache-manifest.4c080099c6ba545dab2555d7bb98dfc5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "87bbe2a30c680eafa274",
+    "url": "/Waldo/static/css/main.748d0375.chunk.css"
+  },
+  {
+    "revision": "87bbe2a30c680eafa274",
+    "url": "/Waldo/static/js/main.b28edafc.chunk.js"
+  },
+  {
+    "revision": "80016d5aca171a2ff2fe",
+    "url": "/Waldo/static/js/runtime~main.a44d539b.js"
+  },
+  {
+    "revision": "312d097172ae349fbaab",
+    "url": "/Waldo/static/js/2.8816eee3.chunk.js"
+  },
+  {
+    "revision": "8fd1daaa505856af0b2b7809b8e4589e",
+    "url": "/Waldo/static/media/map1.8fd1daaa.jpg"
+  },
+  {
+    "revision": "cbae0d7d7b6c36e0fe492e9e2d6840ce",
+    "url": "/Waldo/static/media/map2.cbae0d7d.jpg"
+  },
+  {
+    "revision": "8ac4e68b5f52eb5cace64375d882b4e9",
+    "url": "/Waldo/static/media/map3.8ac4e68b.jpg"
+  },
+  {
+    "revision": "bb8d0277e30b555dc3fccaf9c1858b63",
+    "url": "/Waldo/static/media/map4.bb8d0277.jpg"
+  },
+  {
+    "revision": "5a817682473a396812fb411f79df7595",
+    "url": "/Waldo/static/media/map5.5a817682.jpg"
+  },
+  {
+    "revision": "21c649da5db6ad05a7491e281fa7916f",
+    "url": "/Waldo/static/media/map6.21c649da.jpg"
+  },
+  {
+    "revision": "64a255b0f9ac36d7385fe4ecca35478e",
+    "url": "/Waldo/static/media/LandasansMedium-ALJ6m.64a255b0.otf"
+  },
+  {
+    "revision": "41f79afef3da102141fe5b23d63d507a",
+    "url": "/Waldo/static/media/QuartzoBold-W9lv.41f79afe.ttf"
+  },
+  {
+    "revision": "4e17f7d0a304791dda972e704f008707",
+    "url": "/Waldo/index.html"
+  }
+];
